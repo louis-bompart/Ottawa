68eb3064a6eb07c902b93bf414b96acb134ff2e5
BOMPART Louis <louis.bompart@devinci.fr>
2015-11-23 06:23:55 +0100

    Documentation of DataImporter

--- src/main/java/DataImporter.java
+++ src/main/java/DataImporter.java
@@ -15,16 +15,27 @@ import java.util.stream.Stream;
 public class DataImporter {
     private MongoCollection<Document> mongoCollection;
 
+    /**
+     * @param target :  At index 0, name of targeted DB.
+     *               At index 1, name of targeted collection.
+     * @param source :  Path to the JSON source file.
+     */
     public DataImporter(String[] target, String source) {
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase mongoDatabase = mongoClient.getDatabase(target[0]);
         mongoCollection = mongoDatabase.getCollection(target[1]);
+
         try {
             readFile(source);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * @param fileName : Path to the JSON source file.
+     * @throws IOException : exception excepted if the file doesn't exist.
+     */
     private void readFile(String fileName) throws IOException {
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
             stream.forEach(this::importData);
@@ -32,6 +43,10 @@ public class DataImporter {
             e.printStackTrace();
         }
     }
+
+    /**
+     * @param string : single JSON document.
+     */
     private void importData(String string) {
         Document document = Document.parse(string);
         mongoCollection.insertOne(document);
